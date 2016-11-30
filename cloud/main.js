122d5f9038ce267c63178036c62aa9822dc58e5b
eddie-thp <eduardo_thp@yahoo.com.br>
2016-11-30 02:27:45 -0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -10,21 +10,37 @@ Parse.Cloud.define('pushRequestNotification', function(request, response) {
   var requestObjectId = params.requestObjectId;
   var notificationTitle = params.notificationTitle;
   var notificationBody = params.notificationBody;
+  
+  Parse.Cloud.httpRequest({
+    method: 'POST',
+    url: 'https://fcm.googleapis.com/fcm/send',
+    headers: {
+      'Authorization': 'key=' + process.env.FCM_TOKEN,
+      'Content-Type': 'application/json'
+    },
+    body: {
+      notication: {
+      title: notificationTitle,
+      body: notificationBody
+    },
+    to: toUserFcmToken,
+    data: {
+      requestObjectId: requestObjectId
+    }
+  }).then(function(httpResponse) {
+    console.log("### PUSH OK: " + httpResponse.text);
+  }, function(httpResponse) {
+    console.error('Request failed with response code ' + httpResponse.status);
+    console.error('Response ' + httpResponse.text);
+  });
 
+  /*
   // use to custom tweak whatever payload you wish to send
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo("deviceType", "android");
 
   var payload = {
-    "notication": {
-      "title": notificationTitle,
-      "body": notificationBody
-    },
-    "to": toUserFcmToken,
-    "data": {
-      "requestObjectId": requestObjectId
-    }
-  };
+    */
   
   /*
   payload.notification = {};
